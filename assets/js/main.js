6e0ab3df9deb49ccc0702a25cefcff62fa5e82e4
Sulub <sulubsulub06@gmail.com>
2026-06-07 18:01:02 -0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const obs = new IntersectionObserver((entries) => {
+  entries.forEach(e => { if (e.isIntersecting) e.target.classList.add('visible'); });
+}, { threshold: 0.08 });
+document.querySelectorAll('.fade-up').forEach(el => obs.observe(el));
